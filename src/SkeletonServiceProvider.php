658326f22ad9cd4179ee0b437a2057a3bd1faada
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-08-27 01:05:32 +0330

    refactor(provider) : Redefining the skelton service provider

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -2,24 +2,50 @@
 
 namespace VendorName\Skeleton;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 use VendorName\Skeleton\Commands\SkeletonCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class SkeletonServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+     /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('skeleton')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_migration_table_name_table')
-            ->hasCommand(SkeletonCommand::class);
+  
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+ public function boot(): void
+    {
+        // Load the package views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
+
+        // Publishing is only available when running from the console
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        // Publish the package configuration file
+        $this->publishes([
+            __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+        ], ['skeleton', 'skeleton-config']);
+
+        // Publish the package views
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/skeleton'),
+        ], ['skeleton', 'skeleton-views']);
+
+        // Publish the package migrations
+        $this->publishesMigrations([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], ['skeleton', 'skeleton-migrations']);
+
+        // Register the package Artisan commands
+        $this->commands([
+            SkeletonCommand::class
+        ]);
     }
 }
